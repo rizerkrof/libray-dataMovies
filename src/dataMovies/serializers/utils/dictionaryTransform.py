1099f4ef18a067238988b9e8af39d7ed2b06d145
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2022-11-05 22:58:51 +0100

    make nested_disct_to_list pure (without side effects)

--- src/dataMovies/serializers/utils/dictionaryTransform.py
+++ src/dataMovies/serializers/utils/dictionaryTransform.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 def nested_dict_to_list(dictionary, item_to_transform, new_item_key):
-    if not item_to_transform in dictionary:
-        return dictionary
-    dictionary[item_to_transform] = [{new_item_key: k, **v} for k, v in dictionary[item_to_transform].items()]
-    return dictionary
+    transformedDictionary = dictionary.copy()
+    if not item_to_transform in transformedDictionary:
+        return transformedDictionary
+    transformedDictionary[item_to_transform] = [{new_item_key: k, **v} for k, v in transformedDictionary[item_to_transform].items()]
+    return transformedDictionary
