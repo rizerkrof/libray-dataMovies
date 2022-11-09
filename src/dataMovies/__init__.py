a7befe08e627f288f439a6c6a7356ebc0bf84a42
rizerkrof <34008166+rizerkrof@users.noreply.github.com>
2022-11-09 09:16:55 +0100

    small fixes

--- src/dataMovies/__init__.py
+++ src/dataMovies/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from .dataMovies import search_movie_reviews_df
-from .dataMovies import search_movies_reviews_df
-from .dataMovies import search_movie_users_ratings_df
-from .dataMovies import search_movies_users_ratings_df
+from .dataMoviesFeatures import search_movie_reviews_df
+from .dataMoviesFeatures import search_movies_reviews_df
+from .dataMoviesFeatures import search_movie_users_ratings_df
+from .dataMoviesFeatures import search_movies_users_ratings_df
